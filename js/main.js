d6d7e2ef5dc4547033e7c56bbe4ad4b7032836c6
Dusan Stevanovic <duledu24@gmail.com>
2019-01-13 20:09:00 +0100

    index back to top under 400px

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ $(".back-to-top").click(function () {
   $("html, body").animate({scrollTop:0}, 1000)
 
 });
+$(".back-to-top1").click(function () {
+  $("html, body").animate({scrollTop:0}, 1000)
+
+});
